1c3d6fbcf814f75b737d8e6d4c1bf983fa464564
elton-costa <tonrc1@hotmail.com>
2021-11-26 15:01:52 -0300

    Create colors and gradient in component

--- lib/main.dart
+++ lib/main.dart
@@ -15,22 +15,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Vamos cozinhar?'),
-      ),
-      body: Center(
-        child: Text('Navegar é preciso!!'),
-      ),
-    );
-  }
-}
