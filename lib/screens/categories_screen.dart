a26abae083c0e3c6458606233a90b36f6fb5f6d0
elton-costa <tonrc1@hotmail.com>
2021-12-01 18:38:20 -0300

    Create screens favorite_screen and tabs_screen

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -5,22 +5,17 @@ import '../data/dummy_data.dart';
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Vamos cozinhar?'),
+    return GridView(
+      padding: const EdgeInsets.all(25),
+      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
       ),
-      body: GridView(
-        padding: const EdgeInsets.all(25),
-        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20,
-        ),
-        children: DummyCategories.map((cat) {
-          return CategoryItem(cat);
-        }).toList(),
-      ),
+      children: DummyCategories.map((cat) {
+        return CategoryItem(cat);
+      }).toList(),
     );
   }
 }
---
+++ lib/screens/tabs_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'categories_screen.dart';
+import 'favorite_screen.dart';
+
+class TabScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Vamos cozinhar?'),
+          bottom: TabBar(
+            tabs: [
+              Tab(
+                icon: Icon(Icons.category),
+                text: 'Categorias',
+              ),
+              Tab(
+                icon: Icon(Icons.favorite),
+                text: 'Favoritos',
+              ),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            CategoriesScreen(),
+            FavoriteScreen(),
+          ],
+        ),
+      ),
+    );
+  }
+}
